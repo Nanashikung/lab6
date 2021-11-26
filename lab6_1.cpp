455bb226dad0f7dbe28085d7c72b96ba5dcc94c1
Nanashikung <nata.station@hotmail.com>
2021-11-26 16:57:59 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,18 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int even=0,odd=0;
+    int input;
+    while(input != 0){
+        cout << "Enter an integer: ";
+        cin >> input;
+        if(input%2==0 && input!=0){
+            even+=1;
+        }else if(input%2!=0 && input!=0){
+            odd+=1;
+        }
+    }
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
