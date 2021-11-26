fe4a3c656836a4accea2405d1c492b48533119ea
Nanashikung <nata.station@hotmail.com>
2021-11-26 17:49:10 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include<cmath>
 
 using namespace std;
+
+double deg2rad(double d){
+    return d/ 180*M_PI;
+}
+
+double rad2deg(double r){
+    return r / M_PI * 180;
+}
+
+double findXComponent(double x1,double x2,double a1x,double a2x){
+    return (x1*cos(a1x))+(x2*cos(a2x));
+}
+
+double findYComponent(double y1,double y2,double a1y,double a2y){
+    return (y1*sin(a1y))+(y2*sin(a2y));
+}
+
+double pythagoras(double xsum,double ysum){
+    return sqrt((pow(xsum,2)+pow(ysum,2)));
+}
+
+void showResult(double sum1,double sum2){
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+    cout << "Length of the resultant vector = "<< sum1 <<endl;
+    cout << "Direction of the resultant vector (deg) = "<< sum2<<endl;
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+}
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
